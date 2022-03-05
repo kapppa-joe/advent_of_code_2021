c50efc4485345347fc30547f6a4e40d5890bad4d
Joe Fong <joe.fong@madetech.com>
2022-03-05 23:35:37 +0000

    add solution to day 4 part A

--- lib/puzzle_day04.rb
+++ lib/puzzle_day04.rb
@@ -1,6 +1,9 @@
 class Bingo
+  attr_reader :boards
+
   def initialize(input_text)
     @bingo_numbers, board_data_array = parse_input_text(input_text)
+    @next_number_index = 0
     @boards = board_data_array.map { |board_data| BingoBoard.new(board_data) }
   end
 
@@ -26,22 +29,78 @@ class Bingo
   end
 
   def boards_won
-    [2]
+    (0...@boards.length).filter do |board_number|
+      @boards[board_number].has_won?
+    end
   end
 
-  def board_score(_board_number)
-    4512
+  def tick
+    next_number = @bingo_numbers.fetch(@next_number_index)
+    @boards.each do |board|
+      board.mark_number(next_number)
+    end
+    @next_number_index += 1
+  end
+
+  def board_score(board_number)
+    @boards[board_number].score
+  end
+
+  def find_part_a_solution
+    tick while boards_won.empty?
+    board_score(boards_won.first)
   end
 end
 
 class BingoBoard
-  attr_reader :numbers
+  attr_reader :numbers, :marked_positions, :numbers_marked
 
   def initialize(numbers)
     @numbers = numbers
+    @marked_positions = Array.new(25, false)
+    @numbers_marked = []
   end
 
-  def marked_positions
-    Array.new(25, false)
+  def mark_number(num)
+    return false unless @numbers.include?(num)
+
+    index = @numbers.index(num)
+    @marked_positions[index] = true
+    @numbers_marked << num
   end
+
+  def has_won?
+    0.step(24, 5) do |start_of_row|
+      return true if marked_positions[start_of_row, 5].all?(true)
+    end
+
+    (0..4).each do |start_of_column|
+      return true if start_of_column.step(24, 5).all? do |cell|
+        marked_positions[cell] == true
+      end
+    end
+
+    false
+  end
+
+  def score
+    return 0 unless has_won?
+
+    sum_of_unmarked_numbers = @numbers.sum - @numbers_marked.sum
+    @numbers_marked.last * sum_of_unmarked_numbers
+  end
+end
+
+
+
+
+if __FILE__ == $PROGRAM_NAME
+  require_relative './utils'
+  input_array = read_input_file(4, 'string')
+
+  part_a_solution = Bingo.new(input_array).find_part_a_solution
+  puts "solution for part A: #{part_a_solution}"
+
+  # part_b_solution = find_life_support_rating(input_array)
+  # puts "solution for part B: #{part_b_solution}"
 end
--- spec/puzzle_day04_spec.rb
+++ spec/puzzle_day04_spec.rb
@@ -86,10 +86,60 @@ describe Bingo do
       expect(bingo.parse_input_text(input)).to eql expected_output
     end
   end
+
+  describe '#tick' do
+    it 'mark a bingo number in all boards' do
+      bingo.boards.each do |board|
+        expect(board).to receive(:mark_number).with(7)
+      end
+      bingo.tick
+
+      bingo.boards.each do |board|
+        expect(board).to receive(:mark_number).with(4)
+      end
+      bingo.tick
+    end
+  end
+
+  describe '#boards_won' do
+    it 'return an empty array when game start' do
+      expect(bingo.boards_won).to eql []
+    end
+
+    it 'return the value as expected in example case' do
+      12.times do
+        bingo.tick
+      end
+      expect(bingo.boards_won).to eql [2]
+    end
+
+    it 'return the won boards correctly in another test case' do
+      test_case = EXAMPLE_INPUT.clone
+      test_case[0] = '22, 13, 17, 11, 0, 3, 15, 2'
+      bingo = described_class.new(test_case)
+      8.times { bingo.tick }
+
+      expect(bingo.boards_won).to eql [0, 1]
+    end
+  end
+
+  describe '#find_part_a_solution' do
+    it 'solve the example case correctly' do
+      expect(bingo.find_part_a_solution).to eql 4512
+    end
+  end
 end
 
 describe BingoBoard do
-  let(:bingo_board) { described_class.new((0..24).to_a) }
+  test_data = [
+    22, 13, 17, 11, 0,
+    8,  2, 23,  4, 24,
+    21, 9, 14, 16, 7,
+    6, 10, 3, 18,  5,
+    1, 12, 20, 15, 19
+  ]
+
+  let(:bingo_board) { described_class.new(test_data) }
 
   it 'stores a board represented by an array of 25 numbers' do
     expect(bingo_board.numbers).to be_an(Array)
@@ -101,6 +151,117 @@ describe BingoBoard do
     context 'when no number were marked yet' do
       it 'return an array of size 25 filled with false' do
         expect(bingo_board.marked_positions).to eql [false] * 25
+      end
+    end
+  end
+
+  describe '#mark_number' do
+    context 'when this board contain the marked number' do
+      it 'set the corresponding cell of marked_positions as true' do
+        input = [22, 9, 3]
+        input.each do |number|
+          bingo_board.mark_number(number)
+        end
+        marked_positions = bingo_board.marked_positions
+        expect(marked_positions[0]).to eql true
+        expect(marked_positions[11]).to eql true
+        expect(marked_positions[17]).to eql true
+      end
+
+      it 'append the given number to @numbers_marked' do
+        bingo_board.mark_number(22)
+        expect(bingo_board.numbers_marked).to eql [22]
+
+        bingo_board.mark_number(9)
+        expect(bingo_board.numbers_marked).to eql [22, 9]
+      end
+    end
+
+    context 'when this board does not contain the marked number' do
+      it 'do not change @numbers_marked' do
+        expect(bingo_board.numbers_marked).to eql []
+        bingo_board.mark_number(22)
+        bingo_board.mark_number(100)
+        expect(bingo_board.numbers_marked).to eql [22]
+      end
+    end
+  end
+
+  describe '#has_won?' do
+    context 'when no number were marked yet' do
+      it 'return false' do
+        expect(bingo_board.has_won?).to eql false
+      end
+    end
+
+    context 'when in any horizontal rows all numbers were marked' do
+      it 'return true (test case: 1st row)' do
+        numbers = [22, 13, 17, 11, 0]
+        numbers.each do |num|
+          bingo_board.mark_number(num)
+        end
+
+        expect(bingo_board.has_won?).to eql true
+      end
+
+      it 'return true (test case: 5th row' do
+        bingo_board.numbers[20, 5].each do |num|
+          bingo_board.mark_number(num)
+        end
+
+        expect(bingo_board.has_won?).to eql true
+      end
+    end
+
+    context 'when in any vertical columns all numbers were marked' do
+      it 'return true (1st column)' do
+        numbers = [22, 8, 21, 6, 1]
+        numbers.each do |num|
+          bingo_board.mark_number(num)
+        end
+
+        expect(bingo_board.has_won?).to eql true
+      end
+
+      it 'return true (5th column)' do
+        numbers = [0, 24, 7, 5, 19]
+        numbers.each do |num|
+          bingo_board.mark_number(num)
+        end
+
+        expect(bingo_board.has_won?).to eql true
+      end
+    end
+  end
+
+  describe '#score' do
+    context 'when the board has not won yet' do
+      it 'returns 0' do
+        expect(bingo_board.score).to eql 0
+      end
+    end
+
+    context 'when the board has won' do
+      it 'return the sum of all unmarked number times the last marked number' do
+        numbers = [13, 17, 11, 0, 22] # number in the 1st row
+        numbers.each do |num|
+          bingo_board.mark_number(num)
+        end
+
+        expected_output = bingo_board.numbers[5, 20].sum * 22
+
+        expect(bingo_board.score).to eql expected_output
+      end
+
+      it 'return the sum of all unmarked number times the last marked number (another test case)' do
+        numbers = [17, 23, 14, 0, 24, 7, 5, 19]
+        numbers.each do |num|
+          bingo_board.mark_number(num)
+        end
+
+        expected_output = (bingo_board.numbers.sum - numbers.sum) * 19
+
+        expect(bingo_board.score).to eql expected_output
       end
     end
   end
