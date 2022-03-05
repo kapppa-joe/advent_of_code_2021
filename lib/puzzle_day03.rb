0990fefbd9ff27a5625192a4be19dd7dd4872317
Joe Fong <joe.fong@madetech.com>
2022-03-05 11:33:55 +0000

    add solution to day 3 part B

--- lib/puzzle_day03.rb
+++ lib/puzzle_day03.rb
@@ -1,6 +1,6 @@
 def analyse_diagnostic_report(input_array)
-  gamma_rate = calculate_gamma_rate(input_array)
-  epsilon_rate = calculate_epsilon_rate(gamma_rate, input_array[0].length)
+  gamma_rate = find_gamma_rate(input_array)
+  epsilon_rate = find_epsilon_rate(gamma_rate, input_array[0].length)
   {
     gamma_rate: gamma_rate,
     epsilon_rate: epsilon_rate,
@@ -9,8 +9,8 @@ def analyse_diagnostic_report(input_array)
 end
 
 def find_life_support_rating(input_array)
-  oxygen_generator_rating = calc_oxygen_generator_rating(input_array)
-  co2_scrubber_rating = calc_co2_scrubber_rating(input_array)
+  oxygen_generator_rating = find_oxygen_generator_rating(input_array)
+  co2_scrubber_rating = find_co2_scrubber_rating(input_array)
   {
     oxygen_generator_rating: oxygen_generator_rating,
     co2_scrubber_rating: co2_scrubber_rating,
@@ -18,46 +18,79 @@ def find_life_support_rating(input_array)
   }
 end
 
-def calculate_gamma_rate(input_array)
+def find_gamma_rate(input_array)
   most_frequent_bits(input_array).to_i(2)
 end
 
-def calculate_epsilon_rate(gamma_rate, binary_length)
+def find_epsilon_rate(gamma_rate, binary_length)
   (2 ** binary_length) - 1 - gamma_rate
 end
 
-def calc_oxygen_generator_rating(input_array)
-  23
+def find_oxygen_generator_rating(input_array)
+  repeated_filter(input_array).to_i(2)
 end
 
-def calc_co2_scrubber_rating(input_array)
-  10
+def least_frequent_single_bit(current_bits)
+  most_freq_bit = most_frequent_single_bit(current_bits)
+  most_freq_bit == '1' ? '0' : '1'
+end
+
+def find_co2_scrubber_rating(input_array)
+  binary_num_result = repeated_filter(input_array) do |current_bits|
+    least_frequent_single_bit(current_bits)
+  end
+  binary_num_result.to_i(2)
+end
+
+def repeated_filter(input_array)
+  bit_length = input_array.first.length
+  current_array = input_array
+
+  bit_length.times do |i|
+    current_bits = current_array.map { |binary_num| binary_num[i] }
+    if block_given?
+      chosen_bit = yield current_bits
+    else
+      chosen_bit = most_frequent_single_bit(current_bits)
+    end
+    current_array = current_array.filter { |binary_num| binary_num[i] == chosen_bit }
+    break if current_array.length == 1
+  end
+
+  current_array.first
 end
 
 
-# util functions below 
+# util functions below
 
 def most_frequent_bits(input_array)
   array_of_most_freq_bits =
     input_array.map(&:chars)
                .transpose
-               .map { |bits_at_this_position| find_mode(bits_at_this_position) }
+               .map { |bits| most_frequent_single_bit(bits) }
 
   array_of_most_freq_bits.join('')
 end
 
-def find_mode(array)
+# === deprecated because part B requires to prefer 1 when same frequency ===
+# def find_mode(array)
+#   return nil if array.empty?
+
+#   count_table = array.reduce(Hash.new(0)) do |acc, elem|
+#     acc[elem] += 1
+#     acc
+#   end
+
+#   mode, max_count = count_table.max_by { |_, v| v }
+#   mode
+# end
+
+def most_frequent_single_bit(array)
   return nil if array.empty?
+  return '0' if array.map(&:to_i).sum < array.length / 2.0
 
-  count_table = array.reduce(Hash.new(0)) do |acc, elem|
-    acc[elem] += 1
-    acc
-  end
-
-  mode, max_count = count_table.max_by { |_, v| v }
-  mode
+  '1'
 end
-
 
 if __FILE__ == $PROGRAM_NAME
   require_relative './utils'
@@ -65,4 +98,7 @@ if __FILE__ == $PROGRAM_NAME
 
   part_a_solution = analyse_diagnostic_report(input_array)
   puts "solution for part A: #{part_a_solution}"
+
+  part_b_solution = find_life_support_rating(input_array)
+  puts "solution for part B: #{part_b_solution}"
 end
--- spec/puzzle_day03_spec.rb
+++ spec/puzzle_day03_spec.rb
@@ -19,13 +19,13 @@ describe '#analyse_diagnostic_report' do
   end
 end
 
-describe 'calculate_epsilon_rate' do
+describe 'find_epsilon_rate' do
   it 'return 0 when given 0b01 and 1' do
     gamma_rate = 0b01
     binary_length = 1
     expected_output = 0
 
-    expect(calculate_epsilon_rate(gamma_rate, binary_length)).to eql expected_output
+    expect(find_epsilon_rate(gamma_rate, binary_length)).to eql expected_output
   end
 
   it 'return 1 when given 0b00 and 1' do
@@ -33,7 +33,7 @@ describe 'calculate_epsilon_rate' do
     binary_length = 1
     expected_output = 1
 
-    expect(calculate_epsilon_rate(gamma_rate, binary_length)).to eql expected_output
+    expect(find_epsilon_rate(gamma_rate, binary_length)).to eql expected_output
   end
 
   it 'return 0b11 when given 0b00 and 2' do
@@ -41,7 +41,7 @@ describe 'calculate_epsilon_rate' do
     binary_length = 2
     expected_output = 0b11
 
-    expect(calculate_epsilon_rate(gamma_rate, binary_length)).to eql expected_output
+    expect(find_epsilon_rate(gamma_rate, binary_length)).to eql expected_output
   end
 
   describe 'return the inverted bit of gamma rate with length binary_length' do
@@ -53,7 +53,7 @@ describe 'calculate_epsilon_rate' do
     test_cases.each do |input_values, expected_output|
       gamma_rate, binary_length = input_values
       it "gamma_rate:#{gamma_rate}, binary_length: #{binary_length}, expected_output: #{expected_output}" do
-        expect(calculate_epsilon_rate(gamma_rate, binary_length)).to eql expected_output
+        expect(find_epsilon_rate(gamma_rate, binary_length)).to eql expected_output
       end
     end
   end
@@ -90,52 +90,110 @@ describe '#most_frequent_bits' do
   end
 end
 
-describe '#find_mode' do
-  it 'return nil if given an empty array' do
-    expect(find_mode([])).to eql nil
+describe '#most_frequent_single_bit' do
+  it 'return nil when given an empty array' do
+    input = []
+    expected_output = nil
+
+    expect(most_frequent_single_bit(input)).to eql expected_output
   end
 
-  it 'return 1 if given array [1]' do
-    input = [1]
-    expected_output = 1
+  it "returns '0' when given array ['0']" do
+    input = ['0']
+    expected_output = '0'
 
-    expect(find_mode(input)).to eql expected_output
+    expect(most_frequent_single_bit(input)).to eql expected_output
   end
 
-  it 'return 2 if given array [2]' do
-    input = [2]
-    expected_output = 2
+  it "returns '1' when given array ['1']" do
+    input = ['1']
+    expected_output = '1'
 
-    expect(find_mode(input)).to eql expected_output
+    expect(most_frequent_single_bit(input)).to eql expected_output
   end
 
-  it 'return 2 if given array [1, 2, 2]' do
-    input = [1, 2, 2]
-    expected_output = 2
+  it "returns '1' when given array ['0', '1']" do
+    input = %w[0 1]
+    expected_output = '1'
 
-    expect(find_mode(input)).to eql expected_output
+    expect(most_frequent_single_bit(input)).to eql expected_output
   end
 
-  it 'return 1 if given array [2, 2, 1]' do
-    input = [2, 2, 1]
-    expected_output = 2
-
-    expect(find_mode(input)).to eql expected_output
+  describe 'returns the more frequent bit in the array' do
+    test_cases = [
+      [%w[1 0 1 0 1], '1'],
+      [%w[1 0 1 0 0], '0'],
+      [%w[0 1 0 1 0 1 1], '1'],
+      [%w[0 1 0 1 0 1 0], '0'],
+      [%w[1 1 0 1 0 1 1], '1']
+    ]
+    test_cases.each do |input, expected_output|
+      it "input: #{input}, expected output: #{expected_output}" do
+        expect(most_frequent_single_bit(input)).to eql expected_output
+      end
+    end
   end
 
-  it 'return the mode (most frequent element) in an array' do
-    input = [3, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 3, 2, 2, 1]
-    expected_output = 1
-
-    expect(find_mode(input)).to eql expected_output
+  describe 'return 1 when having same number of 0s and 1s' do
+    test_cases = [
+      [%w[1 0 1 0], '1'],
+      [%w[0 1 0 1 0 1], '1'],
+      [%w[0 0 0 0 1 1 1 1], '1']
+    ]
+    test_cases.each do |input, expected_output|
+      it "input: #{input}, expected output: #{expected_output}" do
+        expect(most_frequent_single_bit(input)).to eql expected_output
+      end
+    end
   end
 end
+
+# describe '#find_mode' do
+#   it 'return nil if given an empty array' do
+#     expect(find_mode([])).to eql nil
+#   end
+
+#   it 'return 1 if given array [1]' do
+#     input = [1]
+#     expected_output = 1
+
+#     expect(find_mode(input)).to eql expected_output
+#   end
+
+#   it 'return 2 if given array [2]' do
+#     input = [2]
+#     expected_output = 2
+
+#     expect(find_mode(input)).to eql expected_output
+#   end
+
+#   it 'return 2 if given array [1, 2, 2]' do
+#     input = [1, 2, 2]
+#     expected_output = 2
+
+#     expect(find_mode(input)).to eql expected_output
+#   end
+
+#   it 'return 1 if given array [2, 2, 1]' do
+#     input = [2, 2, 1]
+#     expected_output = 2
+
+#     expect(find_mode(input)).to eql expected_output
+#   end
+
+#   it 'return the mode (most frequent element) in an array' do
+#     input = [3, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 3, 2, 2, 1]
+#     expected_output = 1
+
+#     expect(find_mode(input)).to eql expected_output
+#   end
+# end
 
 # ======================================
 #                PART B
 # ======================================
 
-xdescribe '#find_life_support_rating' do
+describe '#find_life_support_rating' do
   it 'solve the example correctly' do
     input = %w[00100 11110 10110 10111 10101 01111
                00111 11100 10000 11001 00010 01010]
@@ -146,5 +204,102 @@ xdescribe '#find_life_support_rating' do
     }
 
     expect(find_life_support_rating(input)).to eq expected_output
+  end
+end
+
+describe '#find_oxygen_generator_rating' do
+  it 'solve the example correctly' do
+    input = %w[00100 11110 10110 10111 10101 01111
+               00111 11100 10000 11001 00010 01010]
+    expected_output = 23
+
+    expect(find_oxygen_generator_rating(input)).to eq expected_output
+  end
+end
+
+describe '#find_co2_scrubber_rating' do
+  it 'solve the example correctly' do
+    input = %w[00100 11110 10110 10111 10101 01111
+               00111 11100 10000 11001 00010 01010]
+    expected_output = 10
+
+    expect(find_co2_scrubber_rating(input)).to eq expected_output
+  end
+end
+
+describe '#repeated_filter' do
+  it 'returns the only element if given an array with length of 1' do
+    input = ['1']
+    expected_output = '1'
+
+    expect(repeated_filter(input)).to eql expected_output
+
+    input = ['0']
+    expected_output = '0'
+
+    expect(repeated_filter(input)).to eql expected_output
+  end
+
+  context 'when no block is given' do
+    it "returns '10' if given an array of %w[01 10]" do
+      input = %w[01 10]
+      expected_output = '10'
+
+      expect(repeated_filter(input)).to eql expected_output
+    end
+
+    it "returns '11' if given an array of %w[10 01 11]" do
+      input = %w[10 01 11]
+      expected_output = '11'
+
+      expect(repeated_filter(input)).to eql expected_output
+    end
+
+    describe 'filters the input array repeatedly with most frequent bit, and return the last remaining element' do
+      test_cases = [
+        [%w[11101 00010 11100 01100 11110 11011 01001 10100 00000 00110], '11101'],
+        [%w[00111 01010 01100 01101 10000 00000 10001 10111 10000 01101], '01101']
+      ]
+      test_cases.each do |input, expected_output|
+        it "input: #{input}, expected output: #{expected_output}" do
+          expect(repeated_filter(input)).to eql expected_output
+        end
+      end
+    end
+  end
+
+  context 'when given a block to find least frequent bit' do
+    let(:block) do
+      lambda do |current_bits|
+        most_freq_bit = most_frequent_single_bit(current_bits)
+        most_freq_bit == '1' ? '0' : '1'
+      end
+    end
+
+    it "returns '0' when given array %w[1 0]" do
+      input = %w[1 0]
+      expected_output = '0'
+      expect(repeated_filter(input, &block)).to eql expected_output
+    end
+
+    it "returns '01' if given an array of %w[01 10]" do
+      input = %w[01 10]
+      expected_output = '01'
+
+      expect(repeated_filter(input, &block)).to eql expected_output
+    end
+
+    describe 'filters the input array repeatedly with least frequent bit, and return the last remaining element' do
+      test_cases = [
+        [%w[101 011 110 001], '001'],
+        [%w[0101 0110 0011 1011 0000], '1011'],
+        [%w[10101 00100 01010 11010 01011], '10101']
+      ]
+      test_cases.each do |input, expected_output|
+        it "input: #{input}, expected output: #{expected_output}" do
+          expect(repeated_filter(input, &block)).to eql expected_output
+        end
+      end
+    end
   end
 end
