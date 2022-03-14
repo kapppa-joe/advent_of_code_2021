3466a05c0edafca9b4095676b207c0bade2f14ac
Joe Fong <joe.fong@madetech.com>
2022-03-14 17:11:05 +0000

    add solution to day 12 part B

--- lib/puzzle_day12.rb
+++ lib/puzzle_day12.rb
@@ -19,27 +19,42 @@ module Day12
       next_nodes - small_caves_visited
     end
 
+    def small_cave?(node)
+      node == node.downcase
+    end
+
     def search_paths(graph: @graph, curr: 'start', dest: 'end', visited: [])
       return [[*visited, curr].join(',')] if curr == dest
 
+      visited = visited.clone
+      visited << curr
       next_nodes = possible_next_nodes(graph[curr], visited)
       return [] if next_nodes.empty?
 
-      path = visited.clone
-      path << curr
       recur_results = next_nodes.map do |next_node|
         search_paths(
           graph: graph,
           curr: next_node,
           dest: dest,
-          visited: path
+          visited: visited
         )
       end
       recur_results.flatten
     end
   end
-end
 
+  class PassagePathingWithSecondVisit < PassagePathing
+    def possible_next_nodes(next_nodes, visited = 2)
+      small_caves_visited = visited.filter { |node| small_cave?(node) }
+      has_visited_a_small_cave_twice = small_caves_visited.uniq != small_caves_visited
+
+      caves_not_to_visit = ['start']
+      caves_not_to_visit += small_caves_visited if has_visited_a_small_cave_twice
+
+      next_nodes - caves_not_to_visit
+    end
+  end
+end
 
 if __FILE__ == $PROGRAM_NAME
   require_relative './utils'
@@ -49,6 +64,7 @@ if __FILE__ == $PROGRAM_NAME
   part_a_solution = passage.search_paths.length
   puts "solution for part A: #{part_a_solution}"
 
-  # part_b_solution = scorer.incomplete_strings_middle_score(input_array)
-  # puts "solution for part B: #{part_b_solution}"
+  part_b_instance = Day12::PassagePathingWithSecondVisit.new(input_array)
+  part_b_solution = part_b_instance.search_paths.length
+  puts "solution for part B: #{part_b_solution}"
 end
--- spec/puzzle_day12_spec.rb
+++ spec/puzzle_day12_spec.rb
@@ -1,50 +1,50 @@
 require 'puzzle_day12'
 
+day12_basic_example = %w[
+  start-A
+  start-b
+  A-c
+  A-b
+  b-d
+  A-end
+  b-end
+]
+
+day12_larger_example = %w[
+  dc-end
+  HN-start
+  start-kj
+  dc-start
+  dc-HN
+  LN-dc
+  HN-end
+  kj-sa
+  kj-HN
+  kj-dc
+]
+
+day12_even_larger_example = %w[
+  fs-end
+  he-DX
+  fs-he
+  start-DX
+  pj-DX
+  end-zg
+  zg-sl
+  zg-pj
+  pj-he
+  RW-he
+  fs-DX
+  pj-RW
+  zg-RW
+  start-pj
+  he-WI
+  zg-he
+  pj-fs
+  start-RW
+]
+
 describe Day12::PassagePathing do
-  day12_basic_example = %w[
-    start-A
-    start-b
-    A-c
-    A-b
-    b-d
-    A-end
-    b-end
-  ]
-
-  day12_larger_example = %w[
-    dc-end
-    HN-start
-    start-kj
-    dc-start
-    dc-HN
-    LN-dc
-    HN-end
-    kj-sa
-    kj-HN
-    kj-dc
-  ]
-
-  day12_even_larger_example = %w[
-    fs-end
-    he-DX
-    fs-he
-    start-DX
-    pj-DX
-    end-zg
-    zg-sl
-    zg-pj
-    pj-he
-    RW-he
-    fs-DX
-    pj-RW
-    zg-RW
-    start-pj
-    he-WI
-    zg-he
-    pj-fs
-    start-RW
-  ]
-
   let(:passage) { described_class.new(day12_basic_example) }
 
   describe '#make_graph' do
@@ -188,6 +188,219 @@ describe Day12::PassagePathing do
         passage = described_class.new(day12_even_larger_example)
         expect(passage.search_paths.length).to eql expected_length
       end
+    end
+  end
+end
+
+describe Day12::PassagePathingWithSecondVisit do
+  let(:passage) { described_class.new(day12_basic_example) }
+
+  describe '#possible_next_nodes' do
+    it 'include a small cave in return array if visited 0 times' do
+      next_nodes = %w[a B]
+      visited = []
+      expected_output = %w[a B]
+
+      actual_output = passage.possible_next_nodes(next_nodes, visited)
+      expect(actual_output).to eql expected_output
+    end
+
+    it 'exclude a small cave in return array if visited 2 times' do
+      next_nodes = %w[a B]
+      visited = %w[a a]
+      expected_output = ['B']
+
+      actual_output = passage.possible_next_nodes(next_nodes, visited)
+      expect(actual_output).to eql expected_output
+    end
+
+    it 'include a small cave in return array if visited 1 times' do
+      next_nodes = %w[a B]
+      visited = ['a']
+      expected_output = %w[a B]
+
+      actual_output = passage.possible_next_nodes(next_nodes, visited)
+      expect(actual_output).to eql expected_output
+    end
+
+    it 'exclude start in return array even if it was not visited' do
+      next_nodes = %w[start a B]
+      visited = []
+      expected_output = %w[a B]
+
+      actual_output = passage.possible_next_nodes(next_nodes, visited)
+      expect(actual_output).to eql expected_output
+    end
+
+    it 'exclude start in return array when it was visited once' do
+      next_nodes = %w[start a B]
+      visited = ['start']
+      expected_output = %w[a B]
+
+      actual_output = passage.possible_next_nodes(next_nodes, visited)
+      expect(actual_output).to eql expected_output
+    end
+
+    it 'include a large cave even if already visited once' do
+      next_nodes = %w[start a B]
+      visited = %w[start a a B]
+      expected_output = ['B']
+
+      actual_output = passage.possible_next_nodes(next_nodes, visited)
+      expect(actual_output).to eql expected_output
+    end
+
+    it 'include a large cave even if visited many time' do
+      next_nodes = %w[start a B]
+      visited = %w[start a B a B B]
+      expected_output = ['B']
+
+      actual_output = passage.possible_next_nodes(next_nodes, visited)
+      expect(actual_output).to eql expected_output
+    end
+
+    context 'if alredy visited a small cave twice' do
+      it 'can still visit a small cave if it was not visited yet' do
+        next_nodes = %w[start a c B]
+        visited = %w[start a B a]
+        expected_output = %w[c B]
+
+        actual_output = passage.possible_next_nodes(next_nodes, visited)
+        expect(actual_output).to eql expected_output
+      end
+
+      it 'will not allow a second visit to any small cave' do
+        next_nodes = %w[start aa cc B]
+        visited = %w[start aa B cc aa]
+
+        expected_output = ['B']
+
+        actual_output = passage.possible_next_nodes(next_nodes, visited)
+        expect(actual_output).to eql expected_output
+      end
+    end
+  end
+
+  describe '#search_paths' do
+    it 'can visit a small cave twice' do
+      input = %w[start-a a-b a-end]
+      expected_output = [
+        'start,a,end',
+        'start,a,b,a,end'
+      ]
+      passage = described_class.new(input)
+      actual_output = passage.search_paths
+      expect(actual_output).to match_array expected_output
+    end
+
+    it 'can visit a large cave many time' do
+      input = %w[start-a a-b a-end]
+      expected_output = [
+        'start,a,end',
+        'start,a,b,a,end'
+      ]
+      passage = described_class.new(input)
+      actual_output = passage.search_paths
+      expect(actual_output).to match_array expected_output
+    end
+
+    it 'can visit a single small cave twice' do
+      input = %w[start-a a-C C-end start-b b-C]
+      expected_output = %w[
+        start,a,C,end
+        start,a,C,a,C,end
+        start,a,C,a,C,b,C,end
+        start,a,C,b,C,end
+        start,a,C,b,C,a,C,end
+        start,a,C,b,C,b,C,end
+        start,b,C,end
+        start,b,C,b,C,end
+        start,b,C,b,C,a,C,end
+        start,b,C,a,C,end
+        start,b,C,a,C,b,C,end
+        start,b,C,a,C,a,C,end
+      ]
+
+      passage = described_class.new(input)
+      actual_output = passage.search_paths
+      expect(actual_output).to match_array expected_output
+    end
+
+    it 'will exclude a path which include visiting two small caves each twice' do
+      input = %w[
+        start-kj
+        dc-start
+        HN-start
+        kj-HN
+        kj-dc
+        dc-end
+      ]
+      passage = described_class.new(input)
+      path_not_allowed = 'start,HN,kj,dc,kj,dc,end'
+      actual_output = passage.search_paths
+      expect(actual_output).not_to include(path_not_allowed)
+
+    end
+
+    it 'solves the basic example correctly' do
+      input = day12_basic_example
+      expected_output = %w[
+        start,A,b,A,b,A,c,A,end
+        start,A,b,A,b,A,end
+        start,A,b,A,b,end
+        start,A,b,A,c,A,b,A,end
+        start,A,b,A,c,A,b,end
+        start,A,b,A,c,A,c,A,end
+        start,A,b,A,c,A,end
+        start,A,b,A,end
+        start,A,b,d,b,A,c,A,end
+        start,A,b,d,b,A,end
+        start,A,b,d,b,end
+        start,A,b,end
+        start,A,c,A,b,A,b,A,end
+        start,A,c,A,b,A,b,end
+        start,A,c,A,b,A,c,A,end
+        start,A,c,A,b,A,end
+        start,A,c,A,b,d,b,A,end
+        start,A,c,A,b,d,b,end
+        start,A,c,A,b,end
+        start,A,c,A,c,A,b,A,end
+        start,A,c,A,c,A,b,end
+        start,A,c,A,c,A,end
+        start,A,c,A,end
+        start,A,end
+        start,b,A,b,A,c,A,end
+        start,b,A,b,A,end
+        start,b,A,b,end
+        start,b,A,c,A,b,A,end
+        start,b,A,c,A,b,end
+        start,b,A,c,A,c,A,end
+        start,b,A,c,A,end
+        start,b,A,end
+        start,b,d,b,A,c,A,end
+        start,b,d,b,A,end
+        start,b,d,b,end
+        start,b,end
+      ]
+
+      actual_output = described_class.new(input).search_paths
+      expect(actual_output).to match_array expected_output
+    end
+
+    it 'solves the larger example correctly' do
+      input = day12_larger_example
+      expected_length = 103
+
+      actual_length = described_class.new(input).search_paths.length
+      expect(actual_length).to eql expected_length
+    end
+
+    it 'solves the even larger example correctly' do
+      input = day12_even_larger_example
+      expected_length = 3509
+
+      actual_length = described_class.new(input).search_paths.length
+      expect(actual_length).to eql expected_length
     end
   end
 end
