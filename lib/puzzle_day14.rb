931bd4cc9ddce8489d1c994dfed9e4f1ce5bdfa0
Joe Fong <joe.fong@madetech.com>
2022-03-17 21:56:36 +0000

    add solution to day 14

--- lib/puzzle_day14.rb
+++ lib/puzzle_day14.rb
@@ -20,24 +20,78 @@ module Day14
       { template: template, rules: rules }
     end
 
-    def run_rules(template)
-      result = ''
-      template.length.times do |i|
-        pair = template[i, 2]
-        result << template[i] + polymer_to_insert_between(pair)
-      end
-      result
-    end
-
-    def run_rules_n_times(template, n)
-      n.times.reduce(template) do |polymers|
-        run_rules(polymers)
+    def count_pairs(polymer)
+      number_of_pairs = polymer.length - 1
+      number_of_pairs.times.each_with_object(Hash.new(0)) do |i, hash|
+        pair = polymer[i, 2]
+        hash[pair] += 1
       end
     end
 
-    def polymer_to_insert_between(pair)
-      @rules.include?(pair) ? @rules[pair] : ''
+    def run_rules_on_pairs(pairs_count)
+      new_hash = Hash.new(0)
+      pairs_count.each do |pair, count|
+        new_pairs = make_new_pairs(pair)
+        new_pairs.each { |new_pair| new_hash[new_pair] += count }
+      end
+      new_hash
     end
+
+    def make_new_pairs(pair)
+      return [pair] unless @rules.include?(pair)
+
+      middle_char = @rules[pair]
+      left_char, right_char = pair.chars
+      [left_char + middle_char, middle_char + right_char]
+    end
+
+    def run_rules_on_pairs_n_times(input_pairs, n)
+      n.times.reduce(input_pairs) do |pairs_count|
+        run_rules_on_pairs(pairs_count)
+      end
+    end
+
+    def count_occurrence_from_pairs(starting_polymer, pairs_count)
+      molecules_count = Hash.new(0)
+      pairs_count.each do |pair, count|
+        left_char, right_char = pair.chars
+        molecules_count[left_char] += count
+        molecules_count[right_char] += count
+      end
+
+      add_one_to_start_and_end_chars(starting_polymer, molecules_count) if pairs_count.length > 1
+      reduce_each_count_to_half(molecules_count)
+    end
+
+    def add_one_to_start_and_end_chars(starting_polymer, molecules_count)
+      first_char = starting_polymer[0]
+      last_char = starting_polymer[-1]
+      molecules_count[first_char] += 1
+      molecules_count[last_char] += 1
+    end
+
+    def reduce_each_count_to_half(molecules_count)
+      molecules_count.update(molecules_count) { |_, count| count / 2 }
+    end
+
+    # def run_rules(template)
+    #   result = ''
+    #   template.length.times do |i|
+    #     pair = template[i, 2]
+    #     result << template[i] + polymer_to_insert_between(pair)
+    #   end
+    #   result
+    # end
+
+    # def run_rules_n_times(template, n)
+    #   n.times.reduce(template) do |polymers|
+    #     run_rules(polymers)
+    #   end
+    # end
+
+    # def polymer_to_insert_between(pair)
+    #   @rules.include?(pair) ? @rules[pair] : ''
+    # end
 
     def count_molecules(polymer)
       polymer.chars.each_with_object(Hash.new(0)) do |molecule, count|
@@ -45,17 +99,27 @@ module Day14
       end
     end
 
-    def freq_diff_after_n_step(n, verbose: false)
-      after_10_steps = run_rules_n_times(@starting_template, n)
-      count_result = count_molecules(after_10_steps)
-      most_common = count_result.max_by { |k, v| v }
-      least_common = count_result.min_by { |k, v| v }
-      return most_common.last - least_common.last unless verbose
-      puts "
-        most_common: #{most_common}
-        least_common: #{least_common}
-        diff: #{most_common.last - least_common.last}
-      "
+    # def freq_diff_after_n_step(n, verbose: false)
+    #   after_n_steps = run_rules_n_times(@starting_template, n)
+    #   count_result = count_molecules(after_n_steps)
+    #   most_common = count_result.max_by { |k, v| v }
+    #   least_common = count_result.min_by { |k, v| v }
+    #   return most_common.last - least_common.last unless verbose
+    #   puts "
+    #     most_common: #{most_common}
+    #     least_common: #{least_common}
+    #     diff: #{most_common.last - least_common.last}
+    #   "
+    # end
+
+    def freq_diff_after_n_step_by_pair_counts(n)
+      starting_pairs = count_pairs(@starting_template)
+      after_n_steps = run_rules_on_pairs_n_times(starting_pairs, n)
+      count_result = count_occurrence_from_pairs(@starting_template, after_n_steps)
+      most_common = count_result.max_by { |_k, v| v }
+      least_common = count_result.min_by { |_k, v| v }
+
+      most_common.last - least_common.last
     end
   end
 end
@@ -66,11 +130,11 @@ if __FILE__ == $PROGRAM_NAME
   input_array = read_input_file(14, 'string')
   polymer = Day14::Polymerize.new(input_array)
 
-  part_a_solution = polymer.freq_diff_after_n_step(10)
+  part_a_solution = polymer.freq_diff_after_n_step_by_pair_counts(10)
   puts "solution for part A: #{part_a_solution}"
 
-  after_n_step = polymer.run_rules_n_times(input_array[0], 10)
-  puts after_n_step
+  part_b_solution = polymer.freq_diff_after_n_step_by_pair_counts(40)
+  puts "solution for part B: #{part_b_solution}"
 
   # part_b_instance = Day12::PassagePathingWithSecondVisit.new(input_array)
   # part_b_solution = part_b_instance.search_paths.length
--- spec/puzzle_day14_spec.rb
+++ spec/puzzle_day14_spec.rb
@@ -18,7 +18,7 @@ describe Day14::Polymerize do
     'BB -> N',
     'BC -> B',
     'CC -> N',
-    'CN -> C',
+    'CN -> C'
   ]
 
   describe '::parse_input' do
@@ -51,33 +51,47 @@ describe Day14::Polymerize do
 
   let(:polymer) { described_class.new(day_14_example) }
 
-  describe '#to_pair_count' do
-    it 'count the occurrence of each pair combination in polymer string'
+  describe '#count_pairs' do
+    it 'count the occurrence of each pair combination in polymer string' do
+      input = 'NNCB'
+      expected_output = {
+        'NN' => 1,
+        'NC' => 1,
+        'CB' => 1
+      }
+
+      expect(polymer.count_pairs(input)).to eql expected_output
+    end
+
+    it 'counts multiple occurrence of same pair correctly' do
+      input = 'NBBBCNCCNBBNBNBBCHBHHBCHB'
+      expected_output =
+        { 'NB' => 4, 'BB' => 4, 'BC' => 3, 'CN' => 2, 'NC' => 1,
+          'CC' => 1, 'BN' => 2, 'CH' => 2, 'HB' => 3, 'BH' => 1,
+          'HH' => 1 }
+
+      expect(polymer.count_pairs(input)).to eql expected_output
+    end
   end
 
-  describe '#run_rules' do
+  describe '#run_rules_on_pairs' do
+    it 'return the input if no rules match the input pairs' do
+      polymer = described_class.new(['AB', 'NN -> C'])
+      input = { 'AB' => 1 }
+      expected_output = { 'AB' => 1 }
 
-    it 'return empty string if given empty string' do
-      input = ''
-      expected_output = ''
-      expect(polymer.run_rules(input)).to eql expected_output
+      expect(polymer.run_rules_on_pairs(input)).to eql expected_output
     end
 
-    it 'return NCN if given template NN and rules "NN -> C"' do
-      input_array = [
-        '',
-        '',
-        'NN -> C'
-      ]
+    it 'return {NC => 1, CN => 1} if given { NN => 1} and rule NN -> C' do
+      polymer = described_class.new(['NN', 'NN -> C'])
+      input = polymer.count_pairs('NN')
+      expected_output = { 'NC' => 1, 'CN' => 1 }
 
-      input = 'NN'
-      expected_output = 'NCN'
-
-      polymer = described_class.new(input_array)
-      expect(polymer.run_rules(input)).to eql expected_output
+      expect(polymer.run_rules_on_pairs(input)).to eql expected_output
     end
 
-    describe 'solves the first few steps of example case correctly' do
+    describe 'it solve the first few steps of example case correctly' do
       example_cases = {
         0 => 'NNCB',
         1 => 'NCNBCHB',
@@ -87,63 +101,160 @@ describe Day14::Polymerize do
       }
       4.times do |i|
         it "step #{i + 1}" do
-          input = example_cases[i]
-          expected_output = example_cases[i + 1]
+          input = polymer.count_pairs(example_cases[i])
+          expected_output = polymer.count_pairs(example_cases[i + 1])
 
-          expect(polymer.run_rules(input)).to eql expected_output
+          expect(polymer.run_rules_on_pairs(input)).to eql expected_output
         end
       end
+ 
     end
   end
 
-  describe '#run_rules_n_times' do
+  describe '#run_rules_on_pairs_n_times' do
     it 'insert polymer into the template repeatedly for n times' do
-      input = 'NNCB'
+      input = polymer.count_pairs('NNCB')
       n = 4
-      expected_output = 'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB'
+      expected_output = polymer.count_pairs('NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB')
 
-      expect(polymer.run_rules_n_times(input, n)).to eql expected_output
+      expect(polymer.run_rules_on_pairs_n_times(input, n)).to eql expected_output
+    end
+  end
+
+  # describe '#run_rules' do
+  #   it 'return empty string if given empty string' do
+  #     input = ''
+  #     expected_output = ''
+  #     expect(polymer.run_rules(input)).to eql expected_output
+  #   end
+
+  #   it 'return NCN if given template NN and rules "NN -> C"' do
+  #     input_array = [
+  #       '',
+  #       '',
+  #       'NN -> C'
+  #     ]
+
+  #     input = 'NN'
+  #     expected_output = 'NCN'
+
+  #     polymer = described_class.new(input_array)
+  #     expect(polymer.run_rules(input)).to eql expected_output
+  #   end
+
+  #   describe 'solves the first few steps of example case correctly' do
+  #     example_cases = {
+  #       0 => 'NNCB',
+  #       1 => 'NCNBCHB',
+  #       2 => 'NBCCNBBBCBHCB',
+  #       3 => 'NBBBCNCCNBBNBNBBCHBHHBCHB',
+  #       4 => 'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB'
+  #     }
+  #     4.times do |i|
+  #       it "step #{i + 1}" do
+  #         input = example_cases[i]
+  #         expected_output = example_cases[i + 1]
+
+  #         expect(polymer.run_rules(input)).to eql expected_output
+  #       end
+  #     end
+  #   end
+  # end
+
+  describe '#count_occurrence_from_pairs' do
+    it 'returns the correct count for basic case of "NNCB"' do
+      starting_polymer = "NNCB"
+      pairs_count = polymer.count_pairs(starting_polymer)
+      expected_output = {
+        'N' => 2,
+        'C' => 1,
+        'B' => 1
+      }
+
+      actual_output = polymer.count_occurrence_from_pairs(
+        starting_polymer, pairs_count
+      )
+
+      expect(actual_output).to eql expected_output
     end
 
-    describe 'it produces result matching the known facts about example cases ' do
-      it 'after step 5, it has length 97' do
-        input = 'NNCB'
-        n = 5
-        expected_length = 97
+    describe 'returns the correct count for more complex cases' do
+      make_random_string = -> { ('A'..'H').map { |char| char * rand(20) }.join.chars.shuffle.join }
 
-        actual_output = polymer.run_rules_n_times(input, n)
-        expect(actual_output.length).to eql expected_length
-      end
+      test_strings = ['NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB']
+      5.times { test_strings << make_random_string.call }
 
-      it 'after step 10, it has length 3073, and the count of each type of polymer match given numbers' do
-        input = 'NNCB'
-        n = 10
-        expected_length = 3073
-        expected_occurrence = {
-          'B' => 1749,
-          'C' => 298,
-          'H' => 161,
-          'N' => 865
-        }
+      test_strings.each do |test_string|
+        it "test_string: #{test_string}" do
+          input = polymer.count_pairs(test_string)
+          actual_output = polymer.count_occurrence_from_pairs(
+            test_string, input
+          )
 
-        actual_output = polymer.run_rules_n_times(input, n)
-        expect(actual_output.length).to eql expected_length
-        expected_occurrence.each do |molecule, number|
-          expect(actual_output.count(molecule)).to eql number
+          expect(actual_output).to be_a(Hash)
+          actual_output.each do |key, value|
+            expect(test_string.count(key)).to eql value
+          end
         end
       end
     end
   end
 
-  describe '#freq_diff_after_n_step' do
+  # describe '#run_rules_n_times' do
+  #   it 'insert polymer into the template repeatedly for n times' do
+  #     input = 'NNCB'
+  #     n = 4
+  #     expected_output = 'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB'
+
+  #     expect(polymer.run_rules_n_times(input, n)).to eql expected_output
+  #   end
+
+  #   describe 'it produces result matching the known facts about example cases ' do
+  #     it 'after step 5, it has length 97' do
+  #       input = 'NNCB'
+  #       n = 5
+  #       expected_length = 97
+
+  #       actual_output = polymer.run_rules_n_times(input, n)
+  #       expect(actual_output.length).to eql expected_length
+  #     end
+
+  #     it 'after step 10, it has length 3073, and the count of each type of polymer match given numbers' do
+  #       input = 'NNCB'
+  #       n = 10
+  #       expected_length = 3073
+  #       expected_occurrence = {
+  #         'B' => 1749,
+  #         'C' => 298,
+  #         'H' => 161,
+  #         'N' => 865
+  #       }
+
+  #       actual_output = polymer.run_rules_n_times(input, n)
+  #       expect(actual_output.length).to eql expected_length
+  #       expected_occurrence.each do |molecule, number|
+  #         expect(actual_output.count(molecule)).to eql number
+  #       end
+  #     end
+  #   end
+  # end
+
+  # describe '#freq_diff_after_n_step' do
+  #   it 'solves the example case correctly for n = 10' do
+  #     n = 10
+  #     expect(polymer.freq_diff_after_n_step(n)).to eql 1588
+  #   end
+  # end
+
+  describe '#freq_diff_after_n_step_by_pair_counts' do
     it 'solves the example case correctly for n = 10' do
       n = 10
-      expect(polymer.freq_diff_after_n_step(n)).to eql 1588
+      expect(polymer.freq_diff_after_n_step_by_pair_counts(n)).to eql 1588
     end
 
-    xit 'solves the example case correctly for n = 40' do
+    it 'solves the example case correctly for n = 40' do
       n = 40
-      expect(polymer.freq_diff_after_n_step(n)).to eql 2188189693529
+      expect(polymer.freq_diff_after_n_step_by_pair_counts(n)).to eql 2_188_189_693_529
     end
   end
 
