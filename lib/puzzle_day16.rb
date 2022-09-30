d2d19557b03dde692eda514608724d849b5e39f4
Joe Fong <joe.fong@madetech.com>
2022-09-30 10:42:43 +0100

    day 16: implementing parsing nested packets

--- lib/puzzle_day16.rb
+++ lib/puzzle_day16.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 module Day16
   class PacketDecoder
     def hex_to_bin(hex_string)
@@ -6,15 +8,23 @@ module Day16
       end.join('')
     end
 
-    def parse_binary_stream(stream)
+    def parse_hex_string(hex_string)
+      stream = Day16::StreamIO.new(hex_to_bin(hex_string))
+      parse_binary_stream(stream)
+    end
+
+    def parse_binary_stream(stream, seek_hex_end: true)
       ver = stream.read(3).to_i(2)
       type = stream.read(3).to_i(2)
 
       if type == 4
-        packet = Day16::Packet.new(ver: ver, type: type)
-        packet.value = parse_literal_packet_value(stream)
-        packet
+        packet = PacketLiteral.new(ver: ver, type: type, value: parse_literal_packet_value(stream))
+      else
+        packet = PacketOperator.new(ver: ver, type: type)
+        parse_subpackets(stream).each { |subpacket| packet.add_subpacket(subpacket) }
       end
+      stream.seek_to_next_hex if seek_hex_end
+      packet
     end
 
     def parse_literal_packet_value(stream)
@@ -25,9 +35,22 @@ module Day16
         return binary_value.to_i(2) if next_bits.start_with?('0')
       end
     end
+
+    def parse_subpackets(stream)
+      length_type = stream.read(1)
+      raise NotImplementedError if length_type == '1'
+
+      subpacket_bits_length = stream.read(15).to_i(2)
+      subpacket_bits = stream.read(subpacket_bits_length)
+      subpacket_stream = StreamIO.new(subpacket_bits)
+
+      subpackets = []
+      subpackets << parse_binary_stream(subpacket_stream, seek_hex_end: false) until subpacket_stream.eof?
+      subpackets
+    end
   end
 
-  class Day16::StreamIO
+  class StreamIO
     attr_reader :pos
 
     def initialize(str)
@@ -52,14 +75,50 @@ module Day16
     def eof?
       @pos >= @string.length
     end
+
+    def seek_to_next_hex
+      @pos += (4 - @pos % 4) if @pos % 4 != 0
+    end
   end
 
-  class Day16::Packet
-    attr_accessor :ver, :type, :value
+  class Packet
+    attr_accessor :ver, :type
 
     def initialize(ver: 0, type: 0)
       @ver = ver
       @type = type
+    end
+  end
+
+  class PacketOperator < Packet
+    attr_reader :subpackets
+
+    def initialize(ver: 0, type: 0)
+      super(ver: ver, type: type)
+      @subpackets = []
+    end
+
+    def add_subpacket(subpacket)
+      raise ArgumentError('argument must be of packet class') unless subpacket.is_a?(Packet)
+
+      @subpackets << subpacket
+    end
+  end
+
+  def self.PacketLiteral(ver, type, value)
+    PacketLiteral.new(ver: ver, type: type, value: value)
+  end
+
+  class PacketLiteral < Packet
+    attr_accessor :ver, :type, :value
+
+    def initialize(ver: 0, type: 0, value: 0)
+      super(ver: ver, type: type)
+      @value = value
+    end
+
+    def ==(other)
+      other.is_a?(PacketLiteral) && @ver == other.ver && @type == other.type && @value == other.value
     end
   end
 end
--- spec/puzzle_day16_spec.rb
+++ spec/puzzle_day16_spec.rb
@@ -27,6 +27,54 @@ describe Day16::PacketDecoder do
       expect(output.type).to eql 4
       expect(output.value).to eql 2021
     end
+
+    it 'consumes the place filling zeros at the end correctly' do
+      input = Day16::StreamIO.new('1101001011111110001010001111')
+      decoder.parse_binary_stream(input)
+
+      expect(input.pos).to eql 24
+    end
+  end
+
+  describe '::parse_hex_string' do
+    it 'can parse a single literal packet' do
+      input = 'D2FE28'
+      output = decoder.parse_hex_string(input)
+
+      expect(output.ver).to eql 6
+      expect(output.type).to eql 4
+      expect(output.value).to eql 2021
+    end
+
+    it 'can parse an operator packet' do
+      input = '38006F45291200'
+      output = decoder.parse_hex_string(input)
+
+      expect(output.ver).to eql 1
+      expect(output.type).to eql 6
+    end
+  end
+
+  describe 'parsing nested packets' do
+    it 'can parse an operator packet of type 0 with subpackets' do
+      input = '38006F45291200'
+      output = decoder.parse_hex_string(input)
+      subpackets = output.subpackets
+
+      expect(subpackets.length).to eql 2
+      expect(subpackets[0]).to eq Day16::PacketLiteral(6, 4, 10)
+      expect(subpackets[1]).to eq Day16::PacketLiteral(2, 4, 20)
+    end
+
+    xit 'can parse an operator packet of type 1 with subpackets' do
+      input = 'EE00D40C823060'
+      output = decoder.parse_hex_string(input)
+      subpackets = output.subpackets
+
+      expect(subpackets.length).to eql 3
+      # expect(subpackets[0]).to eq Day16::PacketLiteral(6, 4, 10)
+      # expect(subpackets[1]).to eq Day16::PacketLiteral(2, 4, 20)
+    end
   end
 end
 
@@ -60,5 +108,18 @@ describe Day16::Packet do
   it 'contains a version number and a type number' do
     expect(packet.ver).to be_an(Integer)
     expect(packet.type).to be_an(Integer)
+  end
+
+  it 'allows equality check between two literal packets' do
+    packet_a = Day16::PacketLiteral(1, 1, 2011)
+    packet_b = Day16::PacketLiteral(2, 1, 2011)
+    packet_c = Day16::PacketLiteral(1, 2, 2011)
+    packet_d = Day16::PacketLiteral(1, 1, 2012)
+    packet_e = Day16::PacketLiteral(1, 1, 2011)
+
+    expect(packet_a).to eq packet_e
+    expect(packet_a).not_to eq packet_b
+    expect(packet_a).not_to eq packet_c
+    expect(packet_a).not_to eq packet_d
   end
 end
