37c822b7b9d4e6bb072c892a0f27276a46f8f043
Joe Fong <joe.fong@madetech.com>
2022-10-07 17:27:13 +0100

    add functions to convert coord between stations. save current progress

--- lib/puzzle_day19.rb
+++ lib/puzzle_day19.rb
@@ -2,6 +2,8 @@ require 'matrix'
 
 module Day19
   class ScannersMapper
+    attr_reader :stations
+
     def self.parse_input_list(input_list)
       current_scanner = nil
 
@@ -16,12 +18,98 @@ module Day19
       end
       grouped_data.delete(nil)
 
-      grouped_data.sort.map {|_, beacons| Scanner.new(beacons)}
+      grouped_data.sort.map { |_, beacons| Scanner.new(beacons) }
+    end
+
+    def initialize(input_list)
+      @stations = self.class.parse_input_list(input_list)
+    end
+
+    def common_beacons_between_stations(a, b)
+      # locate the common beacons between two stations,
+      #  using the fact that inter-beacon distance remain the same no matter observed from which station
+      relation_map = {}
+      stations[a].interbeacon_distances.each_with_index do |dist_a, index_from_a|
+        stations[b].interbeacon_distances.each_with_index do |dist_b, index_from_b|
+          common_interbeacon_distances = dist_a & dist_b
+          relation_map[index_from_a] = index_from_b if common_interbeacon_distances.length >= 11
+        end
+      end
+      relation_map
+    end
+
+    def create_coordinate_convertor(base, neighbour, relation_map)
+      base_station = @stations[base]
+      neighbour_station = @stations[neighbour]
+
+      a, b, c, d = relation_map.take(2).flatten
+      station_a_beacon_1, station_a_beacon_2 = base_station[a], base_station[c]
+      station_b_beacon_1, station_b_beacon_2 = neighbour_station[b], neighbour_station[d]
+
+      distance_vec_station_a = station_a_beacon_2 - station_a_beacon_1
+      distance_vec_station_b = station_b_beacon_2 - station_b_beacon_1
+      rotation_matrix = create_rotation_matrix(distance_vec_station_a, distance_vec_station_b)
+      reverse_rotation_matrix = rotation_matrix.inverse.map(&:to_i)
+
+      station_b_beacon_1_rotated = rotation_matrix * station_b_beacon_1
+      translation_component = station_a_beacon_1 - station_b_beacon_1_rotated
+
+      to_base = ->(vec) { rotation_matrix * vec + translation_component }
+      from_base = ->(vec) { reverse_rotation_matrix * (vec - translation_component)}
+      
+      return to_base, from_base
+    end
+
+    def create_rotation_matrix(vec1, vec2)
+      vec1, vec2 = [vec1, vec2].map { |vec| vec.to_a.flatten }
+
+      rows_of_rotation_matrix = vec1.map do |vec1_component|
+        position_in_vec2 = vec2.map(&:abs).index(vec1_component.abs)
+        raise 'error when trying to create rotation matrix' if position_in_vec2.nil?
+
+        row = [0, 0, 0]
+        row[position_in_vec2] = vec1_component / vec2[position_in_vec2]
+        row
+      end
+
+      Matrix.rows(rows_of_rotation_matrix)
+    end
+
+    def build_stations_map
+      all_combinations = @stations.length.times.to_a.combination(2)
+
+      @inter_station_map = Hash.new { |h, k| h[k] = [] }
+      @beacons_relationship_map = Hash.new { |h, k| h[k] = [] }
+
+      all_combinations.each do |a, b|
+        relation_map = common_beacons_between_stations(a, b)
+        next if relation_map.empty?
+
+        @inter_station_map[a] << b
+        @inter_station_map[b] << a
+        @beacons_relationship_map["#{a}->#{b}"] = relation_map
+      end
+
+      @inter_station_map
+    end
+
+    def beacon_coords_to_string(list_of_vectors)
+      list_of_vectors.map { |vector| vector.to_a.flatten.to_s.tr('[] ', '') }
+    end
+
+    def build_coordinate_convertors
+      puts build_stations_map
+    end
+
+    def build_all_beacons_list
+      build_stations_map
+      build_coordinate_convertors
+      []
     end
   end
 
   class Scanner
-    attr_reader :beacons
+    attr_reader :beacons, :interbeacon_distances
 
     def self.parse_beacon_coord(input_list)
       input_list.map do |coord_string|
@@ -32,6 +120,29 @@ module Day19
 
     def initialize(input_list)
       @beacons = self.class.parse_beacon_coord(input_list)
+      length = @beacons.length
+      @interbeacon_distances = length.times.map { [0] * length }
+      # populate interbeacon distances
+      length.times.to_a.combination(2).each { |a, b| distance_between_beacons(a, b) }
+    end
+
+    def distance_between_beacons(a, b)
+      return @interbeacon_distances[a][b] unless @interbeacon_distances[a][b].zero?
+      return 0 if a == b
+
+      distance = (@beacons[a] - @beacons[b]).to_a.flatten.map { |num| num**2 }.sum
+      @interbeacon_distances[a][b] = distance
+      @interbeacon_distances[b][a] = distance
+    end
+
+    def beacon_coords_to_string
+      @beacons.map { |vector| vector.to_a.flatten.to_s.tr('[] ', '') }
+    end
+
+    def [](key)
+      raise ArgumentError unless key < @beacons.length
+
+      @beacons[key]
     end
   end
 end
--- spec/puzzle_day19_spec.rb
+++ spec/puzzle_day19_spec.rb
@@ -1,53 +1,247 @@
 require 'puzzle_day19'
 require 'matrix'
+require 'utils'
 
-describe Day19::ScannersMapper do
-  def vec(arr)
-    # helper func to input a vector from literal
-    Matrix.column_vector(arr)
-  end
+describe 'Day19' do
+  Day19Examples = load_example_file(19, /\w+:/)
+  Day19SmallerExample = Day19Examples[0]
+  Day19LargerExample = Day19Examples[1]
+  Day19LargerExampleSolution = Day19Examples[2]
 
-  describe '::parse_input_list' do
-    it 'convert the puzzle input into a list of Scanner objects, each populated with initial beacon coord' do
-      input_list = [
-        '--- scanner 0 ---',
-        '404,-588,-901',
-        '528,-643,409',
-        '',
-        '--- scanner 1 ---',
-        '649,640,665',
-        '682,-795,504',
-        '',
-        '--- scanner 2 ---',
-        '-589,542,597',
-        '605,-692,669',
-      ]
+  describe Day19::ScannersMapper do
+    def vec(arr)
+      # helper func to input a vector from literal
+      Matrix.column_vector(arr)
+    end
 
-      output = described_class.parse_input_list(input_list)
-      expect(output.length).to eq 3
-      expect(output).to all be_a(Day19::Scanner)
-      expect(output[0].beacons).to include(vec([404,-588,-901]))
-      expect(output[1].beacons).to include(vec([682,-795,504]))
-      expect(output[2].beacons).to include(vec([-589,542,597]))
+    let(:larger_example) { described_class.new(Day19LargerExample) }
+
+    describe '::parse_input_list' do
+      it 'convert the puzzle input into a list of Scanner objects, each populated with initial beacon coord' do
+        input_list = [
+          '--- scanner 0 ---',
+          '404,-588,-901',
+          '528,-643,409',
+          '',
+          '--- scanner 1 ---',
+          '649,640,665',
+          '682,-795,504',
+          '',
+          '--- scanner 2 ---',
+          '-589,542,597',
+          '605,-692,669'
+        ]
+
+        output = described_class.parse_input_list(input_list)
+        expect(output.length).to eq 3
+        expect(output).to all be_a(Day19::Scanner)
+        expect(output[0].beacons).to include(vec([404, -588, -901]))
+        expect(output[1].beacons).to include(vec([682, -795, 504]))
+        expect(output[2].beacons).to include(vec([-589, 542, 597]))
+      end
+    end
+
+    describe '#common_beacons_between_stations' do
+      it 'returns a hash map of common beacons between two stations' do
+        output = larger_example.common_beacons_between_stations(0, 1)
+        expect(output.length).to eq 12
+        expect(output[0])
+      end
+
+      it 'generates a correct relationship map of beacons' do
+        beacon_coords_of_station_0 = larger_example.stations[0].beacon_coords_to_string
+        beacon_coords_of_station_1 = larger_example.stations[1].beacon_coords_to_string
+
+        given_common_beacons_station_0 = %w[
+          -618,-824,-621
+          -537,-823,-458
+          -447,-329,318
+          404,-588,-901
+          544,-627,-890
+          528,-643,409
+          -661,-816,-575
+          390,-675,-793
+          423,-701,434
+          -345,-311,381
+          459,-707,401
+          -485,-357,347
+        ]
+
+        given_common_beacons_station_1 = %w[
+          686,422,578
+          605,423,415
+          515,917,-361
+          -336,658,858
+          -476,619,847
+          -460,603,-452
+          729,430,532
+          -322,571,750
+          -355,545,-477
+          413,935,-424
+          -391,539,-444
+          553,889,-390
+        ]
+
+        indices_of_station_0 = given_common_beacons_station_0.map do |str|
+          beacon_coords_of_station_0.find_index do |coord|
+            coord.include?(str)
+          end
+        end
+        indices_of_station_1 = given_common_beacons_station_1.map do |str|
+          beacon_coords_of_station_1.find_index do |coord|
+            coord.include?(str)
+          end
+        end
+
+        expected = indices_of_station_0.zip(indices_of_station_1).to_h
+
+        actual = larger_example.common_beacons_between_stations(0, 1)
+        expect(actual).to eq expected
+      end
+
+      it 'returns an empty hash if two station are not neighboured' do
+        base_station = 0
+        neighbour_station = 2
+
+        expected = {}
+        actual = larger_example.common_beacons_between_stations(base_station, neighbour_station)
+
+        expect(actual).to eq expected
+      end
+    end
+
+    describe '#create_coordinate_convertor' do
+      it 'returns convertor functions that interchange coordinates of two stations' do
+        base_station = 0
+        neighbour_station = 1
+        relation_map = { 0 => 3, 1 => 8, 3 => 12, 4 => 1, 5 => 24, 6 => 18, 7 => 10, 9 => 0, 12 => 2, 14 => 5,
+                         19 => 15, 24 => 19 }
+
+        b_to_a, a_to_b = larger_example.create_coordinate_convertor(
+          base_station, neighbour_station, relation_map
+        )
+
+        expect(a_to_b).is_a?(Proc)
+        expect(b_to_a).is_a?(Proc)
+
+        relation_map.each do |base_key, neighbour_key|
+          base_beacon = larger_example.stations[base_station].beacons[base_key]
+          neighbour_beacon = larger_example.stations[neighbour_station].beacons[neighbour_key]
+          expect(a_to_b.call(base_beacon)).to eq neighbour_beacon
+          expect(b_to_a.call(neighbour_beacon)).to eq base_beacon
+        end
+      end
+
+      it 'another example' do
+        base_station = 1
+        neighbour_station = 4
+        relation_map = larger_example.common_beacons_between_stations(base_station, neighbour_station)
+
+        b_to_a, a_to_b = larger_example.create_coordinate_convertor(
+          base_station, neighbour_station, relation_map
+        )
+
+        expect(a_to_b).is_a?(Proc)
+        expect(b_to_a).is_a?(Proc)
+
+        relation_map.each do |base_key, neighbour_key|
+          base_beacon = larger_example.stations[base_station].beacons[base_key]
+          neighbour_beacon = larger_example.stations[neighbour_station].beacons[neighbour_key]
+          expect(a_to_b.call(base_beacon)).to eq neighbour_beacon
+          expect(b_to_a.call(neighbour_beacon)).to eq base_beacon
+        end
+      end
+    end
+
+    describe '#create_rotation_matrix' do
+      it 'create a matrix that denote the orientation change by comparing the same distance vectors observed from two stations' do
+        input = [Matrix[[124], [-55], [1310]], Matrix[[-124], [-55], [-1310]]]
+        expected = Matrix[
+          [-1, 0, 0],
+          [0, 1, 0],
+          [0, 0, -1]
+        ]
+
+        actual = larger_example.create_rotation_matrix(*input)
+        expect(actual).to eq expected
+      end
+
+      it 'can handle case of rotated orientation' do
+        input = [Matrix[[-855], [-1486], [-485]], Matrix[[485], [-855], [1486]]]
+        expected = Matrix[[0, 1, 0], [0, 0, -1], [-1, 0, 0]]
+
+        actual = larger_example.create_rotation_matrix(*input)
+        expect(actual).to eq expected
+      end
+    end
+
+    describe '#build_stations_map' do
+      it 'return the neighbouring relationship of stations' do
+        expected = {
+          0 => [1],
+          1 => [0, 3, 4],
+          2 => [4],
+          3 => [1],
+          4 => [1, 2]
+        }
+
+        actual = larger_example.build_stations_map
+
+        expect(actual).to eq expected
+      end
+    end
+
+    xdescribe '#build_all_beacons_list' do
+      it 'return a list of all beacons relative to scanner 0 with no duplications' do
+        actual = larger_example.build_all_beacons_list
+        expected = Day19LargerExampleSolution
+
+        expect(actual.length).to eq 79
+        # expect(actual).to eq expected
+      end
     end
   end
-end
 
-describe Day19::Scanner do
-  describe '::parse_beacon_coord' do
-    it 'convert a list of coordinates into a list of vectors' do
-      input_list = %w[
-        404,-588,-901
-        528,-643,409
-      ]
-      expected = [
-        Matrix[[404], [-588], [-901]],
-        Matrix[[528], [-643], [409]]
-      ]
+  describe Day19::Scanner do
+    describe '::parse_beacon_coord' do
+      it 'convert a list of coordinates into a list of vectors' do
+        input_list = %w[
+          404,-588,-901
+          528,-643,409
+        ]
+        expected = [
+          Matrix[[404], [-588], [-901]],
+          Matrix[[528], [-643], [409]]
+        ]
 
-      actual = described_class.parse_beacon_coord(input_list)
+        actual = described_class.parse_beacon_coord(input_list)
 
-      expect(actual).to eq expected
+        expect(actual).to eq expected
+      end
+    end
+
+    let(:basicScanner) { described_class.new(Day19SmallerExample[1..6]) }
+
+    describe '#distance_between_beacons' do
+      describe 'return the between two beacons' do
+        it 'basic example' do
+          # distance between -1,-1,1 and -2,-2,2
+
+          expected = 3 # for convenience, don't take the sqaure root
+          actual = basicScanner.distance_between_beacons(0, 1)
+
+          expect(actual).to eq expected
+        end
+
+        it 'basic example 2' do
+          # distance between -1,-1,1 and 5, 6, -4
+
+          expected = 110  # for convenience, don't take the sqaure root
+          actual = basicScanner.distance_between_beacons(0, 4)
+
+          expect(actual).to eq expected
+        end
+      end
     end
   end
 end
